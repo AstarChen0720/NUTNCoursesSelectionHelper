4981d9ba57ebb5ba09f9c112dfe5f6d79d56d073
AstarChen0720 <rstar0720@gmail.com>
2026-02-04 01:26:07 +0800

    上傳chrome extension並提交審核

--- main/wxt.config.ts
+++ main/wxt.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
   manifest: {
-    permissions: ['cookies'], // cookies 權限
+    permissions: ["cookies"], // cookies 權限
     host_permissions: [
-      '*://academics.nutn.edu.tw/*', // 學校網址權限
+      "*://academics.nutn.edu.tw/*", // 學校網址權限
     ],
+
+    // 將所有尺寸都指向同一張 128px 的圖片，瀏覽器會自動縮放
+    icons: {
+      "16": "NUTNCSH.png",
+      "32": "NUTNCSH.png",
+      "48": "NUTNCSH.png",
+      "96": "NUTNCSH.png",
+      "128": "NUTNCSH.png",
+    },
   },
 });
